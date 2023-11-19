f12c937be4f2632557e69b0cb77b1b9c799260e7
tatarize <tatarize@gmail.com>
2023-11-18 23:11:40 -0800

    scaffold

--- zinglplotter/zinglplotter.py
+++ zinglplotter/zinglplotter.py
@@ -516,3 +516,23 @@ def plot_line_width(x0: int, y0: int, x1: int, y1: int, wd: float):
                 break
             err += dx
             y0 += sy
+
+
+"""
+void plotLine3d(int x0, int y0, int z0, int x1, int y1, int z1)
+{
+   int dx = abs(x1-x0), sx = x0<x1 ? 1 : -1;
+   int dy = abs(y1-y0), sy = y0<y1 ? 1 : -1; 
+   int dz = abs(z1-z0), sz = z0<z1 ? 1 : -1; 
+   int dm = max(dx,dy,dz), i = dm; /* maximum difference */
+   x1 = y1 = z1 = dm/2; /* error offset */
+ 
+   for(;;) {  /* loop */
+      setPixel(x0,y0,z0);
+      if (i-- == 0) break;
+      x1 -= dx; if (x1 < 0) { x1 += dm; x0 += sx; } 
+      y1 -= dy; if (y1 < 0) { y1 += dm; y0 += sy; } 
+      z1 -= dz; if (z1 < 0) { z1 += dm; z0 += sz; } 
+   }
+}
+"""
